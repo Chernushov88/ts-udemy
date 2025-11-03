30343b775df7c2d76fc99792cf4453a6915f2c2b
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-03 10:47:18 +0200

    Add TDF

--- typescript-maps/src/main.ts
+++ typescript-maps/src/main.ts
@@ -1,7 +1,31 @@
-import { User } from './User.ts'
-import { Company } from './Company.ts'
+// import { User } from './User.ts'
+// import { Company } from './Company.ts'
+// import type { MapOptions } from 'google.maps'
 
-const user = new User()
-const company = new Company()
-console.log(user)
-console.log(company)
+
+
+
+// const user = new User()
+// const company = new Company()
+// console.log(user)
+// console.log(company)
+// console.log(google)
+
+// export function initMap() {
+//   const mapDiv = document.getElementById('map') as HTMLElement
+
+//   const map = new google.maps.Map(mapDiv, {
+//     zoom: 2,
+//     center: { lat: 0, lng: 0 },
+//   })
+// }
+
+// робимо функцію глобальною, щоб її викликав Google Maps API
+// ;(window as any).initMap = initMap
+
+// declare const google: any
+
+const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 3,
+  center: { lat: 0, lng: 0 }
+})
