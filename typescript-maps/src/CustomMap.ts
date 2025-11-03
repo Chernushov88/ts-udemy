29edd75beddfa2cf5eb95415ac0bc25127e3a074
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-03 11:07:48 +0200

    Add CustomMap

---
+++ typescript-maps/src/CustomMap.ts
@@ -0,0 +1,10 @@
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(mapDivId: string) {
+   this.googleMap = new google.maps.Map(document.getElementById(mapDivId) as HTMLElement, {
+      zoom: 3,
+      center: { lat: 0, lng: 0 }
+    })
+  }
+}
--- typescript-maps/src/main.ts
+++ typescript-maps/src/main.ts
@@ -1,31 +1,10 @@
 // import { User } from './User.ts'
 // import { Company } from './Company.ts'
-// import type { MapOptions } from 'google.maps'
+import { CustomMap } from "./CustomMap";
+
+new CustomMap('map')
 
 
 
 
-// const user = new User()
-// const company = new Company()
-// console.log(user)
-// console.log(company)
-// console.log(google)
 
-// export function initMap() {
-//   const mapDiv = document.getElementById('map') as HTMLElement
-
-//   const map = new google.maps.Map(mapDiv, {
-//     zoom: 2,
-//     center: { lat: 0, lng: 0 },
-//   })
-// }
-
-// робимо функцію глобальною, щоб її викликав Google Maps API
-// ;(window as any).initMap = initMap
-
-// declare const google: any
-
-const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 3,
-  center: { lat: 0, lng: 0 }
-})
