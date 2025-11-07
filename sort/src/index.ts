d4891d70d58f1a8d639178b7d42c80fa2f011a22
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-07 16:47:22 +0200

    Implement NumbersCollection class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
 
-const numberCollection = new NumberCollection([10,-300, -3, 0, 5, -1, 4]);
+const numberCollection = new NumberCollection([10,-300, -3, 0, 5, -1, 144]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 console.log(numberCollection.data);
