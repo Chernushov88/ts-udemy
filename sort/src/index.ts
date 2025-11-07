2f529adc1b9e569a736d31fd0db7d5bac7326cb6
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-07 17:04:12 +0200

    Implement  class CharactersCollection

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numberCollection = new NumberCollection([10,-300, -3, 0, 5, -1, 144]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
+
+const characterCollection = new CharactersCollection('Xaayb');
+const charSorter = new Sorter(characterCollection);
+charSorter.sort();
+
+console.log(characterCollection.data);  
 console.log(numberCollection.data);
