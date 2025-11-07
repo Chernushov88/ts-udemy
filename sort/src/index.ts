c31604465d604b30714d6dd8a9219e9e3f884142
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-07 12:51:58 +0200

    First sorting implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,24 @@
-console.log('here')
+class Sorter {
+  // collection: number[];
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
+
+  constructor(public collection: number[]) {}
+  sort():void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i -1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([4, -1, 5, 3, 8, 1, 2]);
+sorter.sort();
+console.log(sorter.collection);
