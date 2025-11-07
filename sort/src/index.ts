e2c2c7abb96b47391ef50ad55b80a5e216b3e24a
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-07 18:20:09 +0200

    Implement  class LinkedList

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,14 +1,28 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+
+
 
 const numberCollection = new NumberCollection([10,-300, -3, 0, 5, -1, 144]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
+console.log(numberCollection.data);
 
 const characterCollection = new CharactersCollection('Xaayb');
 const charSorter = new Sorter(characterCollection);
 charSorter.sort();
-
 console.log(characterCollection.data);  
-console.log(numberCollection.data);
+
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.add(0);
+
+const listSorter = new Sorter(linkedList);
+listSorter.sort();
+linkedList.print();
