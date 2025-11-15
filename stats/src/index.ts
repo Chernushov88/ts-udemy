97cd136ab29bae8c5ea6bd637f83ab705fa7c1ea
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-15 17:48:26 +0200

    Data analysis

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
-import fs from 'fs';
+fetch('/football.csv')
+  .then(res => res.text())
+  .then(data => {
+    const matches = data
+    .split('\n')
+    .map((row: string): string[] => {
+     return row.split(',');      
+    })
+    let manUnitedWins = 0;
+
+    for (let match of matches) {
+      if (match[1] === 'Man United' && match[5] === 'H') {  
+        manUnitedWins++;  
+      } else if(match[2] ==='Man United' && match[5] === 'A') {
+        manUnitedWins++;
+      }
+    }
+    
+    console.log(`Man United won ${manUnitedWins} games`);
+  })
+  .catch(err => console.error('Error fetching the CSV file:', err));
