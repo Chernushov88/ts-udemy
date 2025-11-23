64e4c3c8f6b69bd114c3afe27620fdaf14740776
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-23 10:29:40 +0200

    Composition misconception

--- stats/src/example.ts
+++ stats/src/example.ts
@@ -1,27 +1,27 @@
 // const add = (a: number, b: number): number => {
 //   return a + b;
 // }
-
 // add(10, 1);
 // add(10, 2);
 // add(10, 3);
 
+/*
+class HoldNumber {
+  data: number = 0;
+}
 
-// class HoldNumber {
-//   data: number = 0;
-// }
+class HoldString {
+  data: string = '0';
+}
 
-// class HoldString {
-//   data: string = '0';
-// }
+const holdNumber = new HoldNumber();
+holdNumber.data = 333;
 
-// const holdNumber = new HoldNumber();
-// holdNumber.data = 333;
+const holdString = new HoldString();
+holdString.data = 'asdfaf';
+*/
 
-// const holdString = new HoldString();
-// holdString.data = 'asdfaf';
-
-class HoldAnything<TypeOfData> {
+/*class HoldAnything<TypeOfData> {
   data: TypeOfData;
 }
 
@@ -30,3 +30,30 @@ holdNumber.data = 111;
 
 const holdString = new HoldAnything<string>();
 holdString.data = 'sadf';
+*/
+
+// Composition misconception
+/*
+const rectangular = (state) => {
+  return {
+    area: () => state.width * state.height
+  };
+};
+
+const openable = (state) => {
+  return {
+    toggleOpen: () => {
+      state.isOpen = !state.isOpen;    
+    } 
+  };
+};
+
+const buildRectangularWindow = (state) => {
+  return Object.assign(state, rectangular(state), openable(state));
+}
+
+const rectangWindow = buildRectangularWindow({
+  height: 30,
+  width: 20,
+  openable: false
+})*/
