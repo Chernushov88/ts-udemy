e37bf43b24e058413e70b0bb051511c7be30c319
Serhii Chernyshov <chernushov88@gmail.com>
2025-11-25 16:56:16 +0200

    Implamentation tailwindcss and settings tailwindcss to project

--- SERVER/dist/routes/loginRoutes.js
+++ SERVER/dist/routes/loginRoutes.js
@@ -4,6 +4,6 @@ exports.router = void 0;
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 exports.router = router;
-router.get('/', (req, res) => {
-    res.send('Hello!');
+router.get('/login', (req, res) => {
+    res.render('login'); // шукає views/login.ejs
 });
